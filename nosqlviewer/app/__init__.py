99d8b962be0225495472d92353320fc75a7da16f
SINUVMATHEW <sinuvadakkel@gmail.com>
2024-12-09 15:01:25 +0530

    env file added for render cloud

--- nosqlviewer/app/__init__.py
+++ nosqlviewer/app/__init__.py
@@ -1,13 +1,16 @@
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, jwt_required
 from .routes.cassandra_routes import cassandra_routes
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key your_jwt_secret_key'  
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
     jwt = JWTManager(app)
     @app.route('/protected', methods=['GET'])
     @jwt_required()
@@ -15,5 +18,4 @@ def create_app():
         return jsonify(message="You have access to this protected route")
 
     app.register_blueprint(cassandra_routes)
-
     return app
